05bc9fba74f702412028762cdb90ce1de0be76d8
Deekshant Yadav <68581632+d33kshant@users.noreply.github.com>
2024-07-05 10:03:46 +0000

    feat: create express server

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello, World")
+import express from "express"
+import mongoose from "mongoose"
+import { config } from "dotenv"
+
+import reviewRouter from "./controllers/review.controller"
+
+config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+
+app.use("/review", reviewRouter)
+app.use("/", (_, res) => {
+    res.send("Hello, World!")
+})
+
+
+mongoose.connect(process.env.DB_URI!)
+.then(function(){
+    console.log("Connected to mongodb database")
+    app.listen(PORT, ()=>console.log("Server started running on port:", PORT))
+})
+.catch(console.log)
