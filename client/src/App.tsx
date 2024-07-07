f16be657878e27f5231ef6c6d133b243159a33f3
Deekshant Yadav <68581632+d33kshant@users.noreply.github.com>
2024-07-07 05:11:05 +0000

    feat: implemented routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,45 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import NavBar from './components/NavBar.tsx'
+import SignInPage from './pages/SignInPage.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
+import { useEffect, useState } from 'react'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/signin",
+    element: <SignInPage />
+  }
+])
+
 function App() {
+  const [user,  setUser] = useState<any>(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch("/api/auth/user")
+      const data = await response.json()
+      if (data) {
+        setUser(data)
+      }
+    }
+    fetchUser()
+    return () => setUser(null)
+  }, [])
+
   return (
-    <div>Hello, World</div>
+    <AuthProvider value={{ user }}>
+      <NavBar />
+      <div className="w-full flex justify-center py-2">
+        <div className="w-full max-w-2xl">
+          <RouterProvider router={router} />
+        </div>
+      </div>
+    </AuthProvider>
   )
 }
 
