a56f9f7fffa6c8806150d7f824364fcd52d04ea8
Deekshant Yadav <68581632+d33kshant@users.noreply.github.com>
2024-07-15 13:39:17 +0000

    feat: implmented signout

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,6 +3,27 @@ import { AuthContext } from "../contexts/AuthContext"
 
 export default function NavBar() {
     const { user } = useContext(AuthContext)
+
+    const signOut = async () => {
+        const response = await fetch(
+            "/api/auth/signout",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+        const data = await response.json()
+        if (data.error) {
+            alert(data.error)
+        }
+        if (data.message) {
+            alert(data.message)
+            window.location.href = '/'
+        }
+    }
+
     return (
         <nav className="w-full flex justify-center px-4 py-2 bg-blue-600">
             <div className="w-full max-w-2xl flex items-center justify-between">
@@ -11,7 +32,7 @@ export default function NavBar() {
                 </div>
                 <div>
                     {!user && <a className="bg-white rounded px-4 py-1 font-semibold" href="/signin">Sign In</a>}
-                    {user && <p>{user.username}</p>}
+                    {user && <button onClick={signOut} className="bg-white rounded px-4 py-1 font-semibold">Sing Out</button>}
                 </div>
             </div>
         </nav>
